ec27f0be3e6bf24bd275797b12fad2362340555d
Sansar Choinyambuu <sansar.choinyambuu@mgb.ch>
2022-06-07 20:52:22 +0200

    export as parquet

--- template.py
+++ template.py
@@ -2,7 +2,7 @@
 
 # TODO 1: complete the code so that it runs
 # TODO 2: add type hints for read_csv method
-# TODO 3: print only Swiss cities
+# TODO 3: export only Swiss cities as parquet file
 
 
 def read_csv(path):
